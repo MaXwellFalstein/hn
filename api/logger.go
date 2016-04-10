d9fc30d48ac41eaf8305a5b20bf32e339596aab3
Kevin Kirsche <Kev.Kirsche@gmail.com>
2016-04-09 21:15:45 -0400

    Make the logger be goroutine safe via mutexs
    Signed-off-by: Kevin Kirsche <Kev.Kirsche@gmail.com>

--- api/logger.go
+++ api/logger.go
@@ -2,28 +2,43 @@ package hnapi
 
 import (
 	"fmt"
+	"sync"
 )
+
+// NewLogger returns a new Hacker News API Logger struct.
+func NewLogger(verbose bool) *Logger {
+	return &Logger{
+		Verbose: verbose,
+		mutex:   new(sync.Mutex),
+	}
+}
 
 // VerbosePrintln allows you to use println while verifying whether you are or
 // are not in verbose mode.
 func (l *Logger) VerbosePrintln(a ...interface{}) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
 	if l.Verbose {
-		fmt.Println(a)
+		fmt.Println(a...)
 	}
 }
 
 // VerbosePrintfln allows you to use printfln while verifying whether you are or
 // are not in verbose mode.
 func (l *Logger) VerbosePrintfln(format string, a ...interface{}) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
 	if l.Verbose {
-		fmt.Printf(format+"\n", a)
+		fmt.Printf(format+"\n", a...)
 	}
 }
 
 // VerbosePrintf allows you to use printf while verifying whether you are or
 // are not in verbose mode.
 func (l *Logger) VerbosePrintf(format string, a ...interface{}) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
 	if l.Verbose {
-		fmt.Printf(format, a)
+		fmt.Printf(format, a...)
 	}
 }
--- api/structsLogger.go
+++ api/structsLogger.go
@@ -1,6 +1,9 @@
 package hnapi
 
+import "sync"
+
 // Logger is the logger used by HN API when exposing verbose mode.
 type Logger struct {
 	Verbose bool
+	mutex   *sync.Mutex
 }
